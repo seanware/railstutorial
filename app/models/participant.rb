f6c69f4330328a554c0079898744ba838d392add
& <pair+carolineartz.fab9@devbootcamp.com>
2014-06-21 15:24:57 -0500

    Add special indication if the participants brings many guests

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,5 +3,20 @@ class Participant < ActiveRecord::Base
   belongs_to :event
   accepts_nested_attributes_for :event, allow_destroy: true
 
+  def popularity_label
+    if guests < 2
+      ""
+    elsif guests < 5
+      "Friendly"
+    elsif guests < 10
+      "Popular"
+    elsif guests < 25
+      "Social Butterfly"
+    else
+      "Pop Icon"
+    end
+  end
+
+
 
 end
