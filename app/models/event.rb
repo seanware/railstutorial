58c9082e241fa2e8c3b9a37dd60418d0ebb08827
& <pair+carolineartz.fab9@devbootcamp.com>
2014-06-21 15:18:15 -0500

    Added utility to count guests and display total participants

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,11 @@ class Event < ActiveRecord::Base
   has_many :participants
   accepts_nested_attributes_for :participants, allow_destroy: true
 
+  def total_participants
+    total = self.participants.count
+    self.participants.each do |participant|
+      total += participant.guests
+    end
+    return total
+  end
 end
