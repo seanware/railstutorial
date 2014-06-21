c17bd6f69864055974a2cd409b53edda014ef7d7
& <pair+carolineartz.fab9@devbootcamp.com>
2014-06-21 14:49:44 -0500

    Can add participants via webapp

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,2 +1,16 @@
 class ParticipantsController < ApplicationController
+  def create
+    @event = Event.find(params[:event_id])
+    @participant = @event.participants.new(participant_params)
+    if @participant.save
+      redirect_to event_url(@event)
+    else
+      redirect_to event_url(@event)
+    end
+  end
+
+  private
+    def participant_params
+      params.require(:participant).permit(:name, :guests)
+    end
 end
