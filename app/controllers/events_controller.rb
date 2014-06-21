526d8c6ce7cad5dbc0bcfbe4111bbdbce5de272a
& <pair+carolineartz.fab9@devbootcamp.com>
2014-06-21 11:28:42 -0500

    Added a form to create a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,20 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params[:event])
+    if @event.save
+      redirect_to events_url
+    else
+      render action: "new"
+    end
+  end
+
+  def show
+  end
 end
