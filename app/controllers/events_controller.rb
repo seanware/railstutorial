778a7ebd2af2ee5414aeaa787ea2a5f196b9eab9
& <pair+carolineartz.fab9@devbootcamp.com>
2014-06-21 13:04:53 -0500

    Implemented the strong_paramaters for the creation of new data

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,7 +8,7 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.new(params[:event])
+    @event = Event.new(event_params)
     if @event.save
       redirect_to events_url
     else
@@ -23,7 +23,7 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
 
-    if @event.update_attributes(params[:event])
+    if @event.update_attributes(event_params)
       redirect_to events_path
     else
       render action: "edit"
@@ -39,4 +39,9 @@ class EventsController < ApplicationController
     @event.destroy
     redirect_to events_url
   end
+
+  private
+    def event_params
+      params.require(:event).permit(:name, :description, :location)
+    end
 end
